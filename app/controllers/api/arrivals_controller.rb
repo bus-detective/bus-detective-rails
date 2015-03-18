31cbca66f246e469c0c7a12ec6ff460e828503d4
Kevin Rockwood <kevin@rockwood.me>
2015-03-18 11:16:31 -0400

    Remove arrivals/index action

--- app/controllers/api/arrivals_controller.rb
+++ app/controllers/api/arrivals_controller.rb
@@ -1,8 +1,4 @@
 class Api::ArrivalsController < ApplicationController
-  def index
-    render json: { arrivals: Metro::Connection.arrivals.all }
-  end
-
   def show
     render json: ScheduledArrivals.for_stop(params[:id])
   end
--- spec/requests/arrivals_request_spec.rb
+++ spec/requests/arrivals_request_spec.rb
@@ -11,17 +11,9 @@ RSpec.describe "arrivals api" do
     allow(Metro::Connection).to receive(:get).and_return(fixture)
   end
 
-  describe "api/arrivals" do
-    it "returns all arrivals" do
-      get '/api/arrivals'
-      expect(json["arrivals"].first["route_id"]).to eq("64")
-    end
-  end
-
   describe "api/arrivals/:id" do
     it "returns arrivals for a given stop id" do
       get '/api/arrivals/HAMBELi'
-      puts json["arrivals"].length
       expect(json["arrivals"].first["route_id"]).to eq(17)
     end
   end

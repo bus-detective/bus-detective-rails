3264297cc9f6de104e877eb47dd371050f2e60f8
Kevin Rockwood <kevin@rockwood.me>
2015-04-18 20:04:33 +0900

    Speed up stops search query

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,6 +1,8 @@
 class Stop < ActiveRecord::Base
   acts_as_mappable(lat_column_name: :latitude, lng_column_name: :longitude)
   has_many :stop_times
+  has_many :trips, through: :stop_times
+  has_many :routes, -> { uniq }, through: :trips
   belongs_to :agency
 
   DIRECTION_LABELS = {
@@ -11,10 +13,6 @@ class Stop < ActiveRecord::Base
     "e" => "eastbound",
     "w" => "westbound",
   }
-
-  def routes
-    Route.joins(trips: :stop_times).where(stop_times: { stop_id: self.id }).uniq.order(:id)
-  end
 
   def direction
     DIRECTION_LABELS[remote_id.split(//).last()]

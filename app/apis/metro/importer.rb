399b487bbb1193391c9fc9948e642e42beb1d00f
Kevin Rockwood <kevin@rockwood.me>
2015-04-18 23:47:04 +0900

    Convert find_or_create methods to block form
    This will only update the records if they are new (hopefully making
    import faster)

--- app/apis/metro/importer.rb
+++ app/apis/metro/importer.rb
@@ -4,19 +4,19 @@ class Metro::Importer
   def initialize(options)
     @endpoint = options.fetch(:endpoint, DEFAULT_ENDPOINT)
     @logger = options.fetch(:logger, Rails.logger)
-    @logger.info("fetching #{@endpoint}")
+    @logger.info("Fetching #{@endpoint}")
   end
 
   def import!
-    @logger.info("step 1/5: importing agency #{agency.name}")
+    @logger.info("Step 1/5: Importing agency #{agency.name}")
     import_agency!
-    @logger.info("step 2/5: importing routes (#{source.routes.size})")
+    @logger.info("Step 2/5: Importing routes (#{source.routes.size})")
     import_routes!
-    @logger.info("step 3/5: importing stops (#{source.stops.size})")
+    @logger.info("Step 3/5: Importing stops (#{source.stops.size})")
     import_stops!
-    @logger.info("step 4/5: importing trips (#{source.trips.size})")
+    @logger.info("Step 4/5: Importing trips (#{source.trips.size})")
     import_trips!
-    @logger.info("step 5/5: importing stop times (#{source.stop_times.size})")
+    @logger.info("Step 5/5: Importing stop times (#{source.stop_times.size})")
     import_stop_times!
     true
   end
@@ -36,70 +36,74 @@ class Metro::Importer
 
   def import_stops!
     source.each_stop do |s|
-      stop = Stop.find_or_create_by(remote_id: s.id, agency: agency)
-      stop.update!({
-        code: s.code,
-        name: s.name,
-        description: s.desc,
-        latitude: s.lat,
-        longitude: s.lon,
-        zone_id: s.zone_id,
-        url: s.url,
-        location_type: s.location_type,
-        parent_station: s.parent_station,
-        timezone: s.timezone,
-        wheelchair_boarding: s.wheelchair_boarding
-      })
+      Stop.find_or_create_by!(remote_id: s.id, agency: agency) do |record|
+        record.attributes = {
+          code: s.code,
+          name: s.name,
+          description: s.desc,
+          latitude: s.lat,
+          longitude: s.lon,
+          zone_id: s.zone_id,
+          url: s.url,
+          location_type: s.location_type,
+          parent_station: s.parent_station,
+          timezone: s.timezone,
+          wheelchair_boarding: s.wheelchair_boarding
+        }
+      end
     end
   end
 
   def import_routes!
     source.each_route do |r|
-      route = Route.find_or_create_by(remote_id: r.id, agency: agency)
-      route.update!({
-        short_name: r.short_name,
-        long_name: r.long_name,
-        description: r.desc,
-        route_type: r.type,
-        url: r.url,
-        color: r.color,
-        text_color: r.text_color
-      })
+      Route.find_or_create_by!(remote_id: r.id, agency: agency) do |record|
+        record.attributes = {
+          short_name: r.short_name,
+          long_name: r.long_name,
+          description: r.desc,
+          route_type: r.type,
+          url: r.url,
+          color: r.color,
+          text_color: r.text_color
+        }
+      end
     end
   end
 
   def import_trips!
     source.each_trip do |t|
-      route = Route.find_or_create_by(remote_id: t.route_id, agency: agency)
-      trip = Trip.find_or_create_by(remote_id: t.id, agency: agency)
-      trip.update!({
-        route: route,
-        service_id: t.service_id,
-        headsign: t.headsign,
-        short_name: t.short_name,
-        direction_id: t.direction_id,
-        block_id: t.block_id,
-        shape_id: t.shape_id,
-        wheelchair_accessible: t.wheelchair_accessible,
-        bikes_allowed: t.instance_variable_get("@bikes_allowed")
-      })
+      route = Route.find_or_create_by!(remote_id: t.route_id, agency: agency)
+      Trip.find_or_create_by!(remote_id: t.id, agency: agency) do |record|
+        record.attributes = {
+          route: route,
+          service_id: t.service_id,
+          headsign: t.headsign,
+          short_name: t.short_name,
+          direction_id: t.direction_id,
+          block_id: t.block_id,
+          shape_id: t.shape_id,
+          wheelchair_accessible: t.wheelchair_accessible,
+          bikes_allowed: t.instance_variable_get("@bikes_allowed")
+        }
+      end
     end
   end
 
   def import_stop_times!
-    source.each_stop_time do |s|
-      stop = Stop.find_or_create_by(remote_id: s.stop_id, agency: agency)
-      trip = Trip.find_or_create_by(remote_id: s.trip_id, agency: agency)
-      stop_time = StopTime.find_or_create_by(stop: stop, trip: trip, agency: agency)
-      stop_time.update!({
-        arrival_time: s.arrival_time,
-        departure_time: s.departure_time,
-        stop_sequence: s.stop_sequence,
-        stop_headsign: s.stop_headsign,
-        pickup_type: s.pickup_type,
-        drop_off_type: s.drop_off_type,
-        shape_dist_traveled: s.shape_dist_traveled
-      })
+    source.each_stop_time do |st|
+      stop = Stop.find_or_create_by!(remote_id: st.stop_id, agency: agency)
+      trip = Trip.find_or_create_by!(remote_id: st.trip_id, agency: agency)
+      StopTime.find_or_create_by!(stop: stop, trip: trip, agency: agency) do |record|
+        record.attributes = {
+          arrival_time: st.arrival_time,
+          departure_time: st.departure_time,
+          stop_sequence: st.stop_sequence,
+          stop_headsign: st.stop_headsign,
+          pickup_type: st.pickup_type,
+          drop_off_type: st.drop_off_type,
+          shape_dist_traveled: st.shape_dist_traveled
+        }
+      end
     end
   end
 

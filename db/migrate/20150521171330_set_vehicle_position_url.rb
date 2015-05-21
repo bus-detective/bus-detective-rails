93ed8070ea2367be3f89238f205fb24f33657f0c
Geoff Lane <geoff@zorched.net>
2015-05-21 14:04:56 -0400

    Forgot real migration

--- db/migrate/20150521171330_set_vehicle_position_url.rb
+++ db/migrate/20150521171330_set_vehicle_position_url.rb
@@ -1,4 +1,5 @@
 class SetVehiclePositionUrl < ActiveRecord::Migration
-  def change
+  def up
+    Agency.where(gtfs_endpoint: "http://www.go-metro.com/uploads/GTFS/google_transit_info.zip").update_all(gtfs_vehicle_positions_url: "http://developer.go-metro.com/TMGTFSRealTimeWebService/vehicle/")
   end
 end

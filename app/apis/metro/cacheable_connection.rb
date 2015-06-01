80a507b163f65202fb8d653e4ab65ca45e1267e0
Geoff Lane <geoff@zorched.net>
2015-05-31 21:54:11 -0400

    Log when external request are made
    This will help us with debugging the Caching if problems occur.
    
    Signed-off-by: Geoff Lane <geoff@zorched.net>

--- app/apis/metro/cacheable_connection.rb
+++ app/apis/metro/cacheable_connection.rb
@@ -2,6 +2,7 @@ module Metro
   class CacheableConnection
     def self.get(url, key, cache_opts)
       Rails.cache.fetch(key, cache_opts) do
+        Rails.logger.warn("Cache expired, fetching: #{url}")
         Connection.get(url)
       end
     end

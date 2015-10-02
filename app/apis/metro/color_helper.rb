deb8c4dc1e080f6863699f2b81e409fb66222e30
Katherine Tornwall <ktornwall@gmail.com>
2015-10-02 11:46:58 -0400

    Handle nil colors, use black text

--- app/apis/metro/color_helper.rb
+++ app/apis/metro/color_helper.rb
@@ -1,10 +1,15 @@
 class Metro::ColorHelper
+  BLACK = "000000"
+  WHITE = "FFFFFF"
+
   def self.text_color_for_bg_color(background_color)
+    return BLACK if background_color.blank?
+
     red = background_color[0..1].hex
     green = background_color[2..3].hex
     blue = background_color[4..5].hex
 
     # This gives a luminosity value from 0 - 255
-    ((0.299 * red + 0.587 * green + 0.114 * blue) > 170) ? "000000" : "FFFFFF"
+    ((0.299 * red + 0.587 * green + 0.114 * blue) > 170) ? BLACK : WHITE
   end
 end
--- spec/apis/metro/color_helper_spec.rb
+++ spec/apis/metro/color_helper_spec.rb
@@ -3,19 +3,23 @@ require 'rails_helper'
 RSpec.describe Metro::ColorHelper do
   describe ".text_color_for_bg_color" do
     it "uses white on top of black" do
-      expect(Metro::ColorHelper.text_color_for_bg_color("000000")).to eq("FFFFFF")
+      expect(Metro::ColorHelper.text_color_for_bg_color(Metro::ColorHelper::BLACK)).to eq(Metro::ColorHelper::WHITE)
     end
 
     it "uses white on top of green" do
-      expect(Metro::ColorHelper.text_color_for_bg_color("008000")).to eq("FFFFFF")
+      expect(Metro::ColorHelper.text_color_for_bg_color("008000")).to eq(Metro::ColorHelper::WHITE)
     end
 
     it "uses black on top of white" do
-      expect(Metro::ColorHelper.text_color_for_bg_color("FFFFFF")).to eq("000000")
+      expect(Metro::ColorHelper.text_color_for_bg_color(Metro::ColorHelper::WHITE)).to eq(Metro::ColorHelper::BLACK)
     end
 
     it "uses black on top of yellow" do
-      expect(Metro::ColorHelper.text_color_for_bg_color("ffdd00")).to eq("000000")
+      expect(Metro::ColorHelper.text_color_for_bg_color("ffdd00")).to eq(Metro::ColorHelper::BLACK)
+    end
+
+    it "uses black for a nil color" do
+      expect(Metro::ColorHelper.text_color_for_bg_color(nil)).to eq(Metro::ColorHelper::BLACK)
     end
   end
 end

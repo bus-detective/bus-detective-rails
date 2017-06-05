63a3322589a53c28d318e0c7a2b618ed4413e9ba
Ben Stafford <bendstafford@gmail.com>
2017-06-05 15:11:26 -0400

    Update SSL challenge response for June 2017

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -15,10 +15,12 @@ class IndexController < ApplicationController
       render text: 'gTrmpuvlhFtL3v0N2Rkhk9GBxkzXsnkfwyf_-XWRsj0.3agPbEGMW8yyXAdNJmtYhleq07pUgmnN1oCrhN9iRwA'
     when "GmEiT2D0UoKH2AHimAjBr45hrmMyMhYhw7hNipFxa-0"
       render text: "GmEiT2D0UoKH2AHimAjBr45hrmMyMhYhw7hNipFxa-0.jOl0WYrEi5GTa1BlXCSMh31NLeYmWZbZA_QaK-ZpnIE"
+    when "t0FKxYEVT4eFdtrbOjyqouQ-KcAwY_2wCjukfQjCc5M"
+      render text: "t0FKxYEVT4eFdtrbOjyqouQ-KcAwY_2wCjukfQjCc5M.jOl0WYrEi5GTa1BlXCSMh31NLeYmWZbZA_QaK-ZpnIE"
     else
       render text: "#{params[:id]}.x2TXuRtPY5PkPL4YMeiKaMl4xBtFrjfOe94AR0Iyg1M"
     end
-  end
+    end
 
   def robots
     @enable_robots = Rails.configuration.ENABLE_ROBOTS
